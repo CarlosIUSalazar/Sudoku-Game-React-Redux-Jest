b5a7526c1de19e42439ff552acc2c2f084e5420a
Carlos Salazar <carlosiusalazar@gmail.com>
2020-09-01 20:56:14 +0900

    Changed file structure

---
+++ build/precache-manifest.9e0bc03777e0158b72554a153d2e20b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e962f15275cdf7a13687bc4481847e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824ed5d09ca1f981857f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f388c5d70026577928d",
+    "url": "/static/js/2.c9c884cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9c884cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824ed5d09ca1f981857f",
+    "url": "/static/js/main.3d24d174.chunk.js"
+  },
+  {
+    "revision": "785612ead79d39e6d0c6",
+    "url": "/static/js/runtime-main.3f6a2940.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { unregister } from './core';
+
+ReactDOM.render(
+  <div>Hello World</div>,
+  document.getElementById('root')
+);
+
+unregister();
